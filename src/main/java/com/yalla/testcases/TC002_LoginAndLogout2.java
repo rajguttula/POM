4648336aac991f3348606a6ce2a93b08545121f6
rajguttula <Raju@Raju-PC>
2019-04-25 22:50:19 +0530

    aa

---
+++ src/main/java/com/yalla/testcases/TC002_LoginAndLogout2.java
@@ -0,0 +1,42 @@
+package com.yalla.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.yalla.pages.LoginPage;
+import com.yalla.testng.api.base.Annotations;
+
+public class TC002_LoginAndLogout2 extends Annotations{
+	
+	@BeforeTest
+	public void setData() {
+		testcaseName = "TC001_LoginAndLogout";
+		testcaseDec = "Login into leaftaps";
+		author = "Gayatri";
+		category = "smoke";
+		excelFileName = "LeadsDataFile";
+	} 
+
+	@Test(dataProvider="fetchData") 
+	public void loginAndLogout(String uName, String pwd) {
+		new LoginPage()
+		.enterUserName(uName)
+		.enterPassWord(pwd) 
+		.clickLogin()
+		.clickLogout();
+		
+		
+		
+		/*LoginPage lp = new  LoginPage();
+		lp.enterUserName();
+		lp.enterPassWord();*/
+		
+	}
+	
+}
+
+
+
+
+
+
